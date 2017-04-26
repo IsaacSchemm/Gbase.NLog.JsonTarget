739a87f11b0af46992279033224ad8fd5dfcd04e
Isaac Schemm <isaacschemm@gmail.com>
2017-04-26 13:43:58 -0500

    Add example application

---
+++ Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using ISchemm.NLog.JsonTarget;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example {
+    class Program {
+        static void Main(string[] args) {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            LogManager.ThrowExceptions = true;
+
+            JsonPostTarget.DefaultUrl = "http://localhost:51478/Log/Post";
+
+            logger.Debug("Debug message");
+            logger.Warn("Warning message");
+            Thread.Sleep(2000);
+
+            try {
+                throw new Exception("Test exception");
+            } catch (Exception e) {
+                logger.Error(e);
+            }
+            Thread.Sleep(1000);
+
+            logger.Trace("Trace message");
+            logger.Info("Info message");
+
+            Console.WriteLine("Complete");
+            Thread.Sleep(1000);
+        }
+    }
+}
